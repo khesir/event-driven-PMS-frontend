b131b288fbdb8ef648e4982448fb9890946c3319
ajtollo <khesir2002@gmail.com>
2024-05-13 18:46:11 +0800

    BUg fix

--- src/pages/PayrollPage.tsx
+++ src/pages/PayrollPage.tsx
@@ -6,14 +6,14 @@ import PayrollDataProvider from '@/context/PayrollProvider';
 export default function PayrollPage() {
   return (
     <div className='flex flex-col gap-5 w-full'>
-
+      <PayrollDataProvider>
         <div className='flex w-full justify-between items-center'>
             <PageTittle title="Payroll"/>
             
           <PayrollPageHeading/>
         </div>
         <PayrollPageTable/>
-
+      </PayrollDataProvider>
     </div>
   )
 }
